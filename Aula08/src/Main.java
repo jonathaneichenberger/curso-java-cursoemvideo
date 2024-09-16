9b51ab3b32aeb0472425fc758da28d5643cdfe93
Jonathan Eichenberger <161581454+jonathaneichenberger@users.noreply.github.com>
2024-09-16 05:06:51 -0300

    create Aula08

--- Aula08/src/Main.java
+++ Aula08/src/Main.java
@@ -1,5 +1,16 @@
 public class Main {
     public static void main(String[] args) {
 
+        Pessoa[] p = new Pessoa[2];
+        Livro[] l = new Livro[3];
+
+        p[0] = new Pessoa("Jonathan", 37, "M");
+        p[1] = new Pessoa("Vania", 37, "F");
+
+        l[0] = new Livro("Aprendendo Java", "José da Silva", 300, p[0]);
+        l[1] = new Livro("Poo para iniciantes", "Pedro Paulo", 500, p[1]);
+        l[2] = new Livro("Java Anvançado", "Maria Candido", 800, p[0]);
+
+        System.out.println(l[0].detalhes());
     }
 }
