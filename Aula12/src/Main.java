9845fc7206a8f82cd80e6f51ac843f60305e0d16
Jonathan Eichenberger <161581454+jonathaneichenberger@users.noreply.github.com>
2024-09-17 09:03:39 -0300

    update Aula012

--- Aula12/src/Main.java
+++ Aula12/src/Main.java
@@ -1,5 +1,21 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Mamifero m = new Mamifero();
+        Reptil r = new Reptil();
+        Peixe p = new Peixe();
+        Ave a = new Ave();
+        Canguru canguru = new Canguru();
+        Cachorro cachorro = new Cachorro();
+        Cobra cobra = new Cobra();
+        Tartaruga tartaruga = new Tartaruga();
+        GoldFish goldfish = new GoldFish();
+        Arara arara = new Arara();
+
+        canguru.locomover();
+        cachorro.locomover();
+        canguru.emitirSom();
+        cachorro.emitirSom();
+
     }
 }
